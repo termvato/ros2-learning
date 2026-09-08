5f0df3da5ea38b2b2df2f1c1757f757d62ee2276
vakhtang inasaridze <inasaridzevato@gmail.com>
2026-09-08 17:47:32 +0200

    publish sensor_msgs/JointState with a stamped header

--- src/my_first_pkg/my_first_pkg/publisher_member_function.py
+++ src/my_first_pkg/my_first_pkg/publisher_member_function.py
@@ -15,23 +15,27 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(JointState, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = JointState()
+        msg.name = ['joint1']
+        msg.position = [0]
+        msg.velocity =[0]
+        msg.effort =[0]
+        msg.header.stamp = self.get_clock().now().to_msg()
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg)
         self.i += 1
 
 
--- src/my_first_pkg/my_first_pkg/subscriber_member_function.py
+++ src/my_first_pkg/my_first_pkg/subscriber_member_function.py
@@ -15,7 +15,7 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 
 class MinimalSubscriber(Node):
@@ -23,14 +23,14 @@ class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            String,
+            JointState,
             'topic',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg)
 
 
 def main(args=None):
